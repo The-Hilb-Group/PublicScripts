6c20ebe55f8f7ed7fcfbff1018ce9c37a62175bf
Matthew Wandell <mwandell@hilbgroup.com>
2025-10-23 14:43:29 -0400

    Add PwC-Apps script to install Workday Studio and Postman

---
+++ AVDImageInstallation/Pools/PwC-Apps.ps1
@@ -0,0 +1,42 @@
+﻿$ProgressPreference = 'SilentlyContinue'
+
+function Get-TempFilePath {
+	param (
+		[Parameter(Mandatory = $true)]
+		[string]$FileName
+	)
+	$TempDirectory = [System.IO.Path]::GetTempPath()
+	return (Join-Path -Path $TempDirectory -ChildPath $FileName)
+}
+
+function Invoke-DownloadAndStartProcess {
+	param (
+		[string]$DownloadURL,
+		[string]$FileName,
+		[string]$Arguments
+	)
+	$ProgressPreference = 'SilentlyContinue'
+	$TempFilePath = Get-TempFilePath -FileName $FileName
+	Invoke-WebRequest -Uri $DownloadURL -OutFile $TempFilePath
+	$ProgressPreference = 'Continue'
+	Start-Process $TempFilePath -ArgumentList $Arguments -Wait
+}
+
+function Install-WorkdayStudio {
+	$WDStudioUrl = "https://studio.myworkdaygadgets.com/downloads/workday-studio-x86_64.exe"
+
+	Invoke-DownloadAndStartProcess -DownloadURL $WDStudioUrl -FileName "WDStudio.exe" -Arguments "/S /J=C:\Program Files\Zulu\zulu-17 /RD /D=$($env:APPDATA)\Studio"
+}
+
+function Install-Postman {
+	$PostmanUrl = "https://dl.pstmn.io/download/latest/win64"
+
+	Invoke-DownloadAndStartProcess -DownloadURL $PostmanUrl -FileName "Postman.exe" -Arguments ""
+}
+
+
+Write-Output "Installing Workday Studio..."
+Install-WorkdayStudio
+
+Write-Output "Installing Postman..."
+Install-Postman
