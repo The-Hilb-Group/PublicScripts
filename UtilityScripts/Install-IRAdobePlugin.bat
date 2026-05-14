24dde80c7ef73df2f0d7272853944ea511154199
mwandell-hilbgroup <157649332+mwandell-hilbgroup@users.noreply.github.com>
2026-05-14 09:55:27 -0400

    Add Install-IRAdobePlugin batch script for downloading and executing the Adobe Plug-in installer

---
+++ UtilityScripts/Install-IRAdobePlugin.bat
@@ -0,0 +1,6 @@
+﻿@echo off
+set "tmp_script=%TEMP%\Install-IRAdobePlugin.ps1"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/The-Hilb-Group/PublicScripts/refs/heads/main/UtilityScripts/Install-IRAdobePlugin.ps1' -OutFile '%tmp_script%'"
+powershell -ExecutionPolicy Bypass -File "%tmp_script%"
+del "%tmp_script%"
+pause
